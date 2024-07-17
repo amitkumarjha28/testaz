247eb705ad117d1a504c7a247ea7b019b67b6cb8
Amit <AMIT.KUMARJHA28@HASHICORP.COM>
2024-07-17 22:53:54 +0530

    commit3

--- main.tf
+++ main.tf
@@ -20,9 +20,45 @@ resource "azurerm_network_interface" "vm_nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.prefix}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "allow-rdp"
+    description                = "allow-rdp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*" 
+  }
+}
+
+# Associate the web NSG with the Subnet
+resource "azurerm_subnet_network_security_group_association" "example" {
+  depends_on=[azurerm_network_security_group.example]
+
+  subnet_id                 = var.subnet_id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# Get a Static Public IP for web
+resource "azurerm_public_ip" "example" {
+  name                = "win-${var.prefix}-vm-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  
+}
 resource "azurerm_windows_virtual_machine" "example" {
   name                  = "${var.prefix}-vm"
   resource_group_name   = var.resource_group_name
@@ -44,6 +80,8 @@ resource "azurerm_windows_virtual_machine" "example" {
 
   admin_username = "adminuser"
   admin_password = "Password1234!"  # Replace with your actual strong password
+
+  
 }
 
 
