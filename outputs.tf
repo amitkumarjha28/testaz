338d9348acdb6c658bd8139fbc07600ed660dfa4
Amit Kumar Jha <152669352+amitkumarjha28@users.noreply.github.com>
2024-09-25 12:15:51 +0530

    adding
    Adding

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,8 @@
 # Outputs file
 output "VM_Name" {
   value = azurerm_windows_virtual_machine.example.name
-#  value = "http://${azurerm_public_ip.catapp-pip.fqdn}"
 }
 
 output "catapp_ip" {
   value = azurerm_windows_virtual_machine.example.private_ip_address
-#  value = "http://${azurerm_public_ip.catapp-pip.ip_address}"
 }
